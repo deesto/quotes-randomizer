9012835724f58754a2d417c012302a0a2b9d70e7
John S. De Stefano Jr <deesto@yahoo.com>
2022-09-29 11:38:14 -0400

    Create index.php
    form for loading random quote

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$font          = "Lucida\ Grande,Verdana,Lucida,Helvetica,Arial,sans-serif";
+$fontsize      = "1.75";
+$quotesource   = "phrases";
+
+echo "<font face=\"$font\" size=\"$fontsize\">";
+$quotes        = file("$quotesource");
+$quote         = rand(0, sizeof($quotes)-1);
+echo $quotes[$quote];
+echo "</font>";
+/*
+echo "<br/>";
+echo "<br/>";
+echo "<form>";
+echo "<input type=\"button\" value=\"More?\" onclick=\"history.go(0)\" />";
+echo "</form>";
+*/
+?>
